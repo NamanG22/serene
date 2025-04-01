72cfcd62db7bcf43a80bfe9468faf01ce4c099d3
Naman <t-naman1.garg@ocltp.com>
2025-04-01 12:06:02 +0530

    production config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  images: {
+    domains: [], // Add any external image domains you need
+    unoptimized: false,
+  },
 };
 
 export default nextConfig;
